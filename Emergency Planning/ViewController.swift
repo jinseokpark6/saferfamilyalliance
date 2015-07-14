97e64ff87e829cff6fca8ace5c30343dcc9ea65d
Jin Seok Park <jinseokpark6@gmail.com>
2015-07-14 13:21:30 -0500

    Initial Commit

---
+++ Emergency Planning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emergency Planning
+//
+//  Created by Jin Seok Park on 2015. 7. 14..
+//  Copyright (c) 2015년 Jin Seok Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
